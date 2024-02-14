99eb6ab6ce186a254b60c31d99fbcfad0998dc41
Muhammad Farhan <engr.farhan@icloud.com>
2024-02-14 12:28:41 +0000

    v1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 // import App1 from "./Apps"
-import "./new.css";
+// import "./new.css";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./Redux";
